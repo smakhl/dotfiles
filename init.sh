9f3e1ed26fce14646898a1843b0d1d7506be6a86
smakhl <38110833+smakhl@users.noreply.github.com>
2024-01-28 09:04:00 +0400

    init

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
-# make notification on macos
-# ls /System/Library/Sounds
-osascript -e 'display notification "Init complete" with title "Success" sound name "Ping.aiff"'
-
+source ./utils.sh
 source ./configs/git.sh
 source ./configs/macos.sh
+
+notify "Done" "Setup complete"
---
+++ utils.sh
@@ -0,0 +1,11 @@
+# make notification on macos
+# example
+# notify "Done" "Setup complete"
+# ls /System/Library/Sounds
+function notify() {
+  local title="$1"
+  local text="$2"
+  osascript -e "display notification \"$text\" with title \"$title\" sound name \"Ping.aiff\""
+}
+
+notify "Done" "Setup complete"
