00d98d42d84623a31e70d3ea8b52b481ad8aa5cb
smakhl <38110833+smakhl@users.noreply.github.com>
2024-01-28 08:44:25 +0400

    init

--- configs/git.sh
+++ configs/git.sh
@@ -5,5 +5,6 @@ git config --global core.editor "code --wait"
 git config --global core.autocrlf input
 git config --global rerere.enabled true
 
-git config --global user.email 38110833+smakhl@users.noreply.github.com
-git config --global user.name smakhl
+# optional
+# git config --global user.email 38110833+smakhl@users.noreply.github.com
+# git config --global user.name smakhl
--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 # make notification on macos
 # ls /System/Library/Sounds
 osascript -e 'display notification "Init complete" with title "Success" sound name "Ping.aiff"'
+
+source ./configs/git.sh
+source ./configs/macos.sh
